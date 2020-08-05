e6741d530b332d16ec7837c9f40ca0ddb6a42560
DrSaladin <59449418+DrSaladin@users.noreply.github.com>
2020-08-05 16:56:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,19 +42,17 @@
     .querySelector('.picture');
 
 
+  var picturesContainer = document.querySelector('.pictures');
+
   function renderPhotoList(num) {
-    for (var i = 0; 0 < num; i++) {
-      function renderPhotoElement() {
-        var photoElement = userPhotoTemplate.cloneNode(true);
+    for (var i = 0; i < num; i++) {
+      var photoElement = userPhotoTemplate.cloneNode(true);
 
-        photoElement.querySelector('img').src = 'photos/' + (i + 1) + '.jpg';
-        photoElement.querySelector('.picture__comments').textContent = getRandomArrayElement(messages);
-        photoElement.querySelector('.picture__likes').textContent = getRandomNumber(MIN_LIKES, MAX_LIKES);
+      photoElement.querySelector('.picture__img').src = 'photos/' + (i + 1) + '.jpg';
+      photoElement.querySelector('.picture__comments').textContent = getRandomArrayElement(messages);
+      photoElement.querySelector('.picture__likes').textContent = getRandomNumber(MIN_LIKES, MAX_LIKES);
 
-
-        //return photoElement;
-
-      }
+      picturesContainer.appendChild(photoElement);
     }
   }
 
