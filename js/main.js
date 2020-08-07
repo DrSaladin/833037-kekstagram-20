b712028b31c766dbb28a1cc93d947c641d7db232
DrSaladin <59449418+DrSaladin@users.noreply.github.com>
2020-08-07 15:58:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@
   var MIN_AVATAR_NUMBER = 1;
   var MAX_AVATAR_NUMBER = 6;
 
+  var MAX_BIG_PHOTO_QUANTITY = 1;
+
+  var bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+
   var messages = [
     "Всё отлично!",
     "В целом всё неплохо. Но не всё.",
@@ -43,6 +48,12 @@
 
   var picturesContainer = document.querySelector('.pictures');
 
+  var smallPhoto = {
+    avatar: "img/avatar-6.svg",
+    message: "В целом всё неплохо. Но не всё.",
+    name: "Артем"
+  }
+
   function createPhotoArray () {
     var photoArray = [];
     for (var i = 0; i < MAX_PHOTO_QUANTITY; i++) {
@@ -65,5 +76,23 @@
   }
 
   renderPhotoCollection(createPhotoArray());
+
+
+  var photoComment = document.querySelector('.social__comment');
+  function createBigPhotoArray () {
+    var bigPhotoArray = [];
+    for (var i = 0; i < MAX_PHOTO_QUANTITY; i++) {
+      var bigPhotoElement = bigPicture.cloneNode(true);
+
+      bigPhotoElement.document.querySelector('.big-picture__img').src = 'img/avatar-1.svg';
+      bigPhotoElement.document.querySelector('likes-count').textContent = getRandomNumber(MIN_LIKES, MAX_LIKES);
+      bigPhotoElement.document.querySelector('.comments-count').textContent = getRandomNumber(MIN_LIKES, MAX_LIKES);
+      bigPhotoElement.document.querySelector('.social__caption').textContent =
+      bigPhotoElement.document.querySelector()
+
+    }
+  }
+
+
 
 }) ();
